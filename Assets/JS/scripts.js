0292a225603c9cf93cbf5c63e329e8440df966e4
MAHFOUDIFAHD <mahfoudifahd55@gmail.com>
2022-12-27 17:05:21 +0100

    ACPKTF-3 editting some colors

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -2,6 +2,7 @@ document.getElementById("note").style.display = "block";
 document.getElementById("page").style.display = "none";
 document.getElementById("div1").style.display = "none";  
 document.getElementById("results").style.display = "none";
+document.getElementById("result-head").style.display = "none";
 
 
 
@@ -20,6 +21,7 @@ progressText[current - 1].classList.add("active");
 nextBtnFirst.addEventListener("click", function(event){
   event.preventDefault();
   document.getElementById("results").style.display = "none";
+  document.getElementById("result-head").style.display = "none";
   document.getElementById("note").style.display = "none";
   document.getElementById("page").style.display = "none";
   document.getElementById("div1").style.display = "block";
@@ -34,6 +36,7 @@ nextBtnFirst.addEventListener("click", function(event){
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
   document.getElementById("results").style.display = "none";
+  document.getElementById("result-head").style.display = "none";
   document.getElementById("note").style.display = "none";
   document.getElementById("page").style.display = "block";
   document.getElementById("div1").style.display = "none";
@@ -45,8 +48,9 @@ nextBtnSec.addEventListener("click", function(event){
 });
 
 submitBtn.addEventListener("click", function(){
-  // document.getElementById("page").style.display = "none";
+  document.getElementById("page").style.display = "none";
   document.getElementById("results").style.display = "block";
+  document.getElementById("result-head").style.display = "block";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
